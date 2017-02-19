4ee7c562c2381801fbf749803d3247ce48d84536
nzt48 <nikolaztodorovic26@gmail.com>
2017-02-20 00:27:50 +0100

    Commit

---
+++ scripts/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rofi -show run -lines 3 -eh 2 -width 100 -padding 800 -opacity "80" -bw 0 -bc "$bg-color" -bg "$bg-color" -fg "$text-color" -hlbg "$bg-color" -hlfg "#9575cd" -font "System San Francisco Display 23"
